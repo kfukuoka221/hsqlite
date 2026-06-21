aa59a07ac3bb1636dcd4b7b9db658598f6af32be
Kohei Fukuoka <key@KoheinoMacBook-Pro.local>
2026-06-21 21:58:41 +0900

    add flake

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Effective Haskell";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            ghc
+            hlint
+            cabal-install
+            haskell-language-server
+          ];
+        };
+      }
+    );
+}
